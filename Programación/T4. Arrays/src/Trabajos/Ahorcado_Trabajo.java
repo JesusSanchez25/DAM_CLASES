01aa968959e1fd3db136b5e15b73d4dd6d8d11ad
JesusSanchez25 <jsus3005@gmail.com>
2023-11-23 23:07:49 +0100

    update

---
+++ Programación/T4. Arrays/src/Trabajos/Ahorcado_Trabajo.java
@@ -0,0 +1,114 @@
+package Trabajos;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Ahorcado_Trabajo {
+    public static void main(String[] args) {
+        //1. (AhorcadoArray) Crear un array de String donde se guarden 20 palabras.
+        // El sistema deberá de seleccionar una de las palabras pertenecientes al array.
+        // Una vez seleccionada se jugará al juego del ahorcado donde la palabra a acertar será la seleccionada.
+        // Para ello las normas son las siguientes:
+        //    - Nada mas arrancar se mostrarán por consola tantos _ como letras tenga la palabra a acertar
+        //    - Se pedirá por pantalla la letra con la que se quiere probar
+        //    - En el caso de acertar se mostrará la letra en la posición correspondiente
+        //    - En el caso de falla se restará una vida
+        //    - El programa continuará hasta que:
+        //    - Acierte la palabra
+        //    - Me quede sin vidas
+
+        Scanner scanner = new Scanner(in);
+        // Todas las palabras posibles
+        String[] palabras = new String[]{"humanidad","pierna","familia","criatura","bosque","manzana","carton",
+                "caballo","obelisco","zarza","botella","fatal","cuerpo","rezar","friccion",};
+
+
+        String palElegida = palabras[(int)(Math.random()* palabras  .length)];
+        ArrayList letrasUsadas = new ArrayList<>();
+        char[] palabraSecreta = palElegida.toCharArray();
+        Character letraElegida;
+        int vidas = 5;
+        int letrasDescubiertas = 0;
+
+
+
+
+        // Asignar a la palabra oculta '_' en todos sus valores
+        for (int i = 0; i < palElegida.length(); i++) {
+            palabraSecreta[i] = '_';
+        }
+        System.out.println("");
+
+        //Ejecuta el juego hasta que las vidas sean 0 o la palabra esté completa
+        do{
+            //Te permite elegir una letra para adivinar en el ahorcado
+            //Si pones la misma letra 2 veces te lo recuerda
+            do {
+                System.out.print("Elige una letra: \t\t");
+                ImprimirPalabraOculta(palabraSecreta);
+                System.out.println("\n\n");
+
+                letraElegida = scanner.next().charAt(0);
+
+                //No deja que repitas letra
+                if (letrasUsadas.contains(letraElegida)){
+                    LimpiarPantalla();
+                    System.out.println("Esa letra ya la has introducido, elige una diferente");
+                }
+            }while (letrasUsadas.contains(letraElegida));
+                letrasUsadas.add(letraElegida);
+
+
+            LimpiarPantalla();
+            //Si aciertas una letra te felicita y se actualiza la palabra oculta
+            if (palElegida.contains(letraElegida.toString())){
+                System.out.println("\nHas acertado");
+
+                for (int i = 0; i < palElegida.length(); i++) {
+                    if(palElegida.charAt(i)==letraElegida){
+                        palabraSecreta[i]=palElegida.charAt(i);
+                        letrasDescubiertas++;
+                    }
+
+                }
+
+
+                //Si fallas pierdes una vida y te avisa de cuantas vidas te quedan
+            } else {
+                vidas--;
+                if (vidas!=0){
+                    System.out.println("\nInténtalo de nuevo, has perdido una vida");
+                    System.out.println("te quedan "+vidas+" vidas");
+                }
+            }
+        } while (vidas!=0 && letrasDescubiertas<palElegida.length());
+
+            //Cuando pierdes todas tus vidas se para el juego y te dice la palabra que era
+        if (vidas==0){System.out.println("Has perdido :(");
+            System.out.println("La palabra era  " + palElegida);}
+        else {
+            System.out.println("Has ganado!!");
+        }
+
+    }
+
+    public static void LimpiarPantalla(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("");
+    }
+}
+
+    public static void ImprimirPalabraOculta(char[] PalSecreta){
+
+        for (int i = 0; i < PalSecreta.length; i++) {
+            System.out.print(PalSecreta[i]);
+        }
+        System.out.println("");
+    }
+}
+
+
+
+
---
+++ Programación/T4. Arrays/src/Trabajos/EjercicioBingo_Trabajo.java
@@ -0,0 +1,230 @@
+package Trabajos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class EjercicioBingo_Trabajo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+        ArrayList carton = new ArrayList<>(30);
+        ArrayList numerosSalidos = new ArrayList<>();
+        int velocidad = -1;
+        boolean lineaRealizada = false;
+        int contadorIntentos = 0;
+
+        //Da números a la cartilla que no se pueden repetir, así hasta 30 números, los coloca automáticamente
+        // por columnas según la decena
+        GenerarCarton(carton);
+
+        //Muestra la cartilla
+        System.out.println("Esta es tu cartilla de bingo:");
+        MostrarCarton(carton);
+        String respuesta;
+
+
+        //Elige la modalidad
+        do {
+            System.out.println("Pulsa R si quieres una partida rápida o L si quieres una partida lenta");
+            respuesta = ""+scanner.next().charAt(0);
+            if (!respuesta.equalsIgnoreCase("R") && !respuesta.equalsIgnoreCase("l")){
+                System.out.println("Inserte una de las opciones");
+            }
+
+            switch (respuesta.toString().toLowerCase()){
+                case "r"-> velocidad=0;
+                case "l" -> velocidad=3;
+            }
+        } while (!respuesta.equalsIgnoreCase("r") && !respuesta.equalsIgnoreCase("l"));
+
+
+        //Empieza el juego, el cual termina cuando el jugador haga bingo
+        do {
+            //Genera un número nuevo de Bingo que no puede haber salido previamente
+            int numeroBingo;
+            
+            do {
+                numeroBingo = (int)((Math.random()*90));
+
+            } while (numerosSalidos.contains(numeroBingo));
+            numerosSalidos.add(numeroBingo);
+
+            //Dice el número que ha salido y lo elimina de la cartilla
+            System.out.println("\n\nEl número que ha tocado es el: "+ numeroBingo);
+            contadorIntentos++;
+            if(carton.contains(numeroBingo))
+            {carton.set(carton.indexOf(numeroBingo),"  ");
+
+            }
+
+            //Muestra el cartón actualizado
+            MostrarCarton(carton);
+
+            //Detecta si ha habido una línea en ese turno
+            if (linea(carton) && !lineaRealizada){
+                lineaRealizada=true;
+                System.out.println("TE HA SALIDO UNA LÍNEA!!");
+                System.out.println("Para continuar pulse -> Y");
+                respuesta = ""+scanner.next().charAt(0);
+
+                while (!respuesta.equalsIgnoreCase("Y")){
+                    System.out.println("Letra incorrecta, cuando esté listo pulse -> Y");
+                    respuesta = ""+scanner.next().charAt(0);
+                }
+            }
+
+            //La cantidad de segundos hasta el siguiente número, depende de la modalidad
+            try {
+                Thread.sleep(velocidad*1100);
+            }
+            catch (Exception e) {
+                System.out.println();
+            }
+
+        } while (!bingo(carton));
+
+
+
+        //BINGO
+        if (bingo(carton)){
+
+            System.out.println("HAS CONSEGUIDO EL PREMIO DEL BINGO!!!");
+            System.out.println( "El bingo ha sido conseguido en: "+contadorIntentos+" intentos");
+        }
+
+
+
+    }
+
+    public static ArrayList GenerarCarton (ArrayList carton){
+        //Genera 3 números en cada decena
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                //Rellena el ArrayList de números aleatorios en pares de 3, ordenados por decenas
+                int numero;
+                do {
+                    numero =(int)(((Math.random()*10)+10*i)); //cuando hace 3 vueltas cambia de decena
+                }while (carton.contains(numero));
+                carton.add(numero);
+
+                //Pone en orden de menor a mayor los números
+                //Empieza siempre en el segundo valor, coge el valor genenrado y lo compara con el valor anterior,
+                // si es menor que este se cambia y vuelve a ejecutarse hasta que sea el menor de estos
+                while ((carton.indexOf(numero)>0) && (numero < (int)(carton.get(carton.indexOf(numero)-1)))){
+                    carton.add(carton.indexOf(numero)-1,numero);
+                    carton.remove(carton.lastIndexOf(numero));
+                }
+            }
+        }
+
+        //Sustituye un número aleatorio en cada fila y lo reemplaza por un espacio
+        for (int k = 0; k < 9; k++) {
+            carton.set((int)((Math.random()*3)+3*k), "  ");
+        }
+
+        // Después, elige 3 filas aleatorias para hacer lo mismo, dejando un total
+        //de 15 números en el cartón y 12 espacios
+        for (int i = 0; i < 3; i++) {
+            int aleatorioColumna= (int)(Math.random()*9);
+            int aleatorioFila = 0;
+            do {
+                //Detecta si ya hay un hueco ahí y si lo hay lo intenta de nuevo con otro número de
+                //la misma columna, es un poco más enrevesado al intentar hacerlo con ArrayList pero
+                //funciona bien (Hice el ejercicio cuando empezabamos a dar ArrayList)
+                aleatorioFila = (int)(Math.random()*3);
+                if (!carton.get((aleatorioColumna*3)+(aleatorioFila)).equals("  ")){
+                    carton.set((aleatorioColumna*3)+(aleatorioFila), "  ");
+                    break;
+                }
+            }while (true);
+
+        }
+
+        return carton;
+    }
+
+    public static void MostrarCarton (ArrayList carton){
+        Object[][] cartonArray = new Object[3][9];
+        //Da valor a un nuevo array multidimensional basado en el cartón para imprimirlo más facilmente
+
+        //Ordena los números por columnas
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 3; j++) {
+                //Introduce los números previamente ordenados en su respectiva celda
+                String numeroBingo = carton.get(j+(3*i)).toString();
+                cartonArray[j][i] = (numeroBingo);
+            }
+        }
+
+        System.out.println(" ╔══════════════════════════════════════╗ ");
+        for (Object[] fila : cartonArray) {
+            System.out.printf(" ║");
+            for (Object numero : fila) {
+                System.out.print("\t" + numero);
+            }
+            System.out.printf("  ║");
+            System.out.println("");
+        }
+
+        System.out.println(" ╚══════════════════════════════════════╝");
+    }
+
+    public static boolean linea (ArrayList carton){
+        String[][] cartonArray = new String[3][9];
+
+        //Da valor a un nuevo array multidimensional basado en el cartón
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                cartonArray[i][j] = carton.get(j+(9*i)).toString();
+            }
+        }
+
+
+        // analiza todas las filas
+        for (int i = 0; i < 3; i++) {
+            int contador = 0;
+
+            //Mira solo la primera columna y si detecta algún espacio procede a analizar
+            //la fila entera
+            if (cartonArray[i][0].equals("  ")){
+                for (int j = 0; j < 9; j++) {
+                    if (cartonArray[i][j].equals("  ")){
+                        contador++;
+                    }
+                }
+                //Si la fila entera está vacia grita línea
+                if (contador==cartonArray[i].length){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+
+
+    }
+
+    public static boolean bingo (ArrayList carton){
+        String numero = carton.get(0).toString();
+        int contador = 0;
+
+        //Revisa del primer número al último en busca de bingo, si encuentra algún
+        //valor que no sea un espacio para automáticamente para mayor eficiencia.
+        while (numero.equals("  ") && contador<carton.size()){
+            numero = carton.get(contador).toString();
+            if(numero.equals("  ")){
+                contador++;}
+        }
+
+        //Si completa el ciclo todos los números serán espacios y habrá bingo
+        if (contador==carton.size()){
+            return true;
+        }
+
+        //Si para antes de llegar a 27 comprobaciones todavía quedará algún número
+        //por tachar y no será bingo
+        return false;
+    }}
