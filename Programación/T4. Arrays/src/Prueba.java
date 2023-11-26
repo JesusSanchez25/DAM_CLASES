8c7eae24131774771ab3273241518cadecf99b96
JesusSanchez25 <jsus3005@gmail.com>
2023-11-26 01:01:57 +0100

    update

---
+++ Programación/T4. Arrays/src/Prueba.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Prueba {
+    public static Object[][] carton = new Object[3][9];
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 3; j++) {
+                //Rellena el ArrayList de números aleatorios en pares de 3, ordenados por decenas
+                int numeroAleatorio;
+                do {
+                    numeroAleatorio = (int)(Math.random()*10+10*i);
+                } while (numeroEsta(numeroAleatorio) || numeroAleatorio==0);
+                carton[j][i] = numeroAleatorio;
+            }}
+
+
+        //Pone en orden de menor a mayor los números
+        for (int j = 0; j < carton[0].length; j++) {
+            for (int i = 0; i < carton.length-1; i++) {
+                if ((int)carton[i+1][j] < (int)carton[i][j]){
+                    Object aux = carton[i][j];
+
+                    carton[i][j] = carton[i+1][j];
+                    carton[i+1][j] = aux;
+
+                    if (i!=0){
+                        i-=2;
+                    }
+                }
+            }
+        }
+
+        //Sustituye un número aleatorio en cada fila y lo reemplaza por un espacio
+        for (int k = 0; k < 9; k++) {
+            carton[(int)(Math.random()*3)][k] = "  ";
+        }
+
+
+        // Después, elige 3 filas aleatorias para hacer lo mismo, dejando un total
+        //de 15 números en el cartón y 12 espacios
+        for (int i = 0; i < 3; i++) {
+            ArrayList<Integer> columnasCambiadas = new ArrayList<>();
+            do {
+                //Detecta si ya hay un hueco ahí y si lo hay lo intenta de nuevo con otro número de
+                //la misma columna hasta que pone el hueco y pasa a otra columna diferente
+                int aleatorioColumna;
+                int aleatorioFila = (int)(Math.random()*3);
+
+                do {
+                    aleatorioColumna= (int)(Math.random()*9);
+                } while (columnasCambiadas.contains(aleatorioColumna));
+
+                if (!carton[aleatorioFila][aleatorioColumna].equals("  ")){
+                    carton[aleatorioFila][aleatorioColumna] = "  ";
+                    columnasCambiadas.add(aleatorioColumna);
+                    break;
+                }
+            }while (true);
+
+        }
+
+
+        MostrarCarton();
+
+
+
+    }
+
+    public static boolean numeroEsta(Object numero){
+        for (Object[] linea : carton) {
+            for (Object casilla : linea) {
+                if (casilla==numero){
+                    return true;
+                }
+            }
+        }
+
+
+        return false;
+    }
+
+    public static void MostrarCarton (){
+        //todo que pida directamente el array multidimensionales
+
+        System.out.println(" ╔══════════════════════════════════════╗ ");
+        for (Object[] fila : carton) {
+            System.out.printf(" ║");
+            for (Object numero : fila) {
+                System.out.print("\t" + numero);
+            }
+            System.out.printf("  ║");
+            System.out.println("");
+        }
+
+        System.out.println(" ╚══════════════════════════════════════╝");
+    }
+
+}
--- Programación/T4. Arrays/src/Trabajos/EjercicioBingo_Trabajo.java
+++ Programación/T4. Arrays/src/Trabajos/EjercicioBingo_Trabajo.java
@@ -1,27 +1,31 @@
 package Trabajos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 import static java.lang.System.in;
 
 public class EjercicioBingo_Trabajo {
+    public static Object[][] carton = new Object[3][9];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
-        ArrayList carton = new ArrayList<>(30);
         ArrayList numerosSalidos = new ArrayList<>();
         int velocidad = -1;
         boolean lineaRealizada = false;
         int contadorIntentos = 0;
 
+
+
         //Da números a la cartilla que no se pueden repetir, así hasta 30 números, los coloca automáticamente
         // por columnas según la decena
-        GenerarCarton(carton);
+        GenerarCarton();
 
         //Muestra la cartilla
         System.out.println("Esta es tu cartilla de bingo:");
-        MostrarCarton(carton);
+        MostrarCarton();
         String respuesta;
 
 
@@ -54,18 +58,19 @@ public class EjercicioBingo_Trabajo {
             //Dice el número que ha salido y lo elimina de la cartilla
             System.out.println("\n\nEl número que ha tocado es el: "+ numeroBingo);
             contadorIntentos++;
-            if(carton.contains(numeroBingo))
-            {carton.set(carton.indexOf(numeroBingo),"  ");
+            //TODO CAMBIAR ESTO PARA QUE DETECTE DEL ARRAYMULTIDIMENSIONAL DE TU CARTÓN
+            tacharNumero(numeroBingo);
 
-            }
 
             //Muestra el cartón actualizado
-            MostrarCarton(carton);
+             MostrarCarton();
 
             //Detecta si ha habido una línea en ese turno
-            if (linea(carton) && !lineaRealizada){
+            //todo cambiar que cartón sea un array Multidimensional
+            if (linea() && !lineaRealizada){
                 lineaRealizada=true;
                 System.out.println("TE HA SALIDO UNA LÍNEA!!");
+                System.out.println("En el turno: "+contadorIntentos);
                 System.out.println("Para continuar pulse -> Y");
                 respuesta = ""+scanner.next().charAt(0);
 
@@ -83,12 +88,12 @@ public class EjercicioBingo_Trabajo {
                 System.out.println();
             }
 
-        } while (!bingo(carton));
+        } while (!bingo());
 
 
 
         //BINGO
-        if (bingo(carton)){
+        if (bingo()){
 
             System.out.println("HAS CONSEGUIDO EL PREMIO DEL BINGO!!!");
             System.out.println( "El bingo ha sido conseguido en: "+contadorIntentos+" intentos");
@@ -98,45 +103,80 @@ public class EjercicioBingo_Trabajo {
 
     }
 
-    public static ArrayList GenerarCarton (ArrayList carton){
-        //Genera 3 números en cada decena
+    public static boolean numeroEsta(Object numero){
+        for (Object[] linea : carton) {
+            for (Object casilla : linea) {
+                if (casilla==numero){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void tacharNumero(Object numero){
+        for (int i = 0; i < carton[0].length; i++) {
+            for (int j = 0; j < carton.length; j++) {
+                if (carton[j][i]==numero){
+                    carton[j][i] = "  ";
+                }
+            }
+        }
+
+
+    }
+
+    public static Object[][] GenerarCarton (){
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 3; j++) {
-
-                //Rellena el ArrayList de números aleatorios en pares de 3, ordenados por decenas
-                int numero;
+                //Rellena el array de números aleatorios en pares de 3, ordenados por decenas
+                int numeroAleatorio;
                 do {
-                    numero =(int)(((Math.random()*10)+10*i)); //cuando hace 3 vueltas cambia de decena
-                }while (carton.contains(numero));
-                carton.add(numero);
+                    numeroAleatorio = (int)(Math.random()*10+10*i);
+                } while (numeroEsta(numeroAleatorio) || numeroAleatorio==0);
+                carton[j][i] = numeroAleatorio;
+            }}
 
-                //Pone en orden de menor a mayor los números
-                //Empieza siempre en el segundo valor, coge el valor genenrado y lo compara con el valor anterior,
-                // si es menor que este se cambia y vuelve a ejecutarse hasta que sea el menor de estos
-                while ((carton.indexOf(numero)>0) && (numero < (int)(carton.get(carton.indexOf(numero)-1)))){
-                    carton.add(carton.indexOf(numero)-1,numero);
-                    carton.remove(carton.lastIndexOf(numero));
+
+        //Pone en orden de menor a mayor los números
+        for (int j = 0; j < carton[0].length; j++) {
+            for (int i = 0; i < carton.length-1; i++) {
+                if ((int)carton[i+1][j] < (int)carton[i][j]){
+                    Object aux = carton[i][j];
+
+                    carton[i][j] = carton[i+1][j];
+                    carton[i+1][j] = aux;
+
+                    if (i!=0){
+                        i-=2;
+                    }
                 }
             }
         }
 
         //Sustituye un número aleatorio en cada fila y lo reemplaza por un espacio
         for (int k = 0; k < 9; k++) {
-            carton.set((int)((Math.random()*3)+3*k), "  ");
+            carton[(int)(Math.random()*3)][k] = "  ";
         }
+
 
         // Después, elige 3 filas aleatorias para hacer lo mismo, dejando un total
         //de 15 números en el cartón y 12 espacios
         for (int i = 0; i < 3; i++) {
-            int aleatorioColumna= (int)(Math.random()*9);
-            int aleatorioFila = 0;
+            ArrayList<Integer> columnasCambiadas = new ArrayList<>();
             do {
                 //Detecta si ya hay un hueco ahí y si lo hay lo intenta de nuevo con otro número de
-                //la misma columna, es un poco más enrevesado al intentar hacerlo con ArrayList pero
-                //funciona bien (Hice el ejercicio cuando empezabamos a dar ArrayList)
-                aleatorioFila = (int)(Math.random()*3);
-                if (!carton.get((aleatorioColumna*3)+(aleatorioFila)).equals("  ")){
-                    carton.set((aleatorioColumna*3)+(aleatorioFila), "  ");
+                //la misma columna hasta que pone el hueco y pasa a otra columna diferente
+                int aleatorioColumna;
+                int aleatorioFila = (int)(Math.random()*3);
+
+                do {
+                    aleatorioColumna= (int)(Math.random()*9);
+                } while (columnasCambiadas.contains(aleatorioColumna));
+
+                if (!carton[aleatorioFila][aleatorioColumna].equals("  ")){
+                    carton[aleatorioFila][aleatorioColumna] = "  ";
+                    columnasCambiadas.add(aleatorioColumna);
                     break;
                 }
             }while (true);
@@ -146,22 +186,10 @@ public class EjercicioBingo_Trabajo {
         return carton;
     }
 
-    public static void MostrarCarton (ArrayList carton){
-        Object[][] cartonArray = new Object[3][9];
-        //Da valor a un nuevo array multidimensional basado en el cartón para imprimirlo más facilmente
-
-        //Ordena los números por columnas
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 3; j++) {
-                //Introduce los números previamente ordenados en su respectiva celda
-                String numeroBingo = carton.get(j+(3*i)).toString();
-                cartonArray[j][i] = (numeroBingo);
-            }
-
-        }
-
+    public static void MostrarCarton (){
+        //Puestra el Object[][] de cartón
         System.out.println(" ╔══════════════════════════════════════╗ ");
-        for (Object[] fila : cartonArray) {
+        for (Object[] fila : carton) {
             System.out.printf(" ║");
             for (Object numero : fila) {
                 System.out.print("\t" + numero);
@@ -173,59 +201,37 @@ public class EjercicioBingo_Trabajo {
         System.out.println(" ╚══════════════════════════════════════╝");
     }
 
-    public static boolean linea (ArrayList carton){
-        String[][] cartonArray = new String[3][9];
+    public static boolean linea (){
 
-        //Da valor a un nuevo array multidimensional basado en el cartón
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 9; j++) {
-                cartonArray[i][j] = carton.get(j+(9*i)).toString();
-            }
-        }
-
-
-        // analiza todas las filas
-        for (int i = 0; i < 3; i++) {
+        //Revisa del primer número al último en busca de bingo, si encuentra algún
+        //valor que no sea un espacio para automáticamente para mayor eficiencia.
+        for (Object[] linea : carton) {
             int contador = 0;
 
-            //Mira solo la primera columna y si detecta algún espacio procede a analizar
-            //la fila entera
-            if (cartonArray[i][0].equals("  ")){
-                for (int j = 0; j < 9; j++) {
-                    if (cartonArray[i][j].equals("  ")){
-                        contador++;
-                    }
+            for (Object numero: linea) {
+                if (!numero.equals("  ")){
+                    break;
                 }
-                //Si la fila entera está vacia grita línea
-                if (contador==cartonArray[i].length){
+                contador++;
+                if (contador==linea.length){
                     return true;
                 }
             }
         }
         return false;
-
-
-
     }
 
-    public static boolean bingo (ArrayList carton){
-        String numero = carton.get(0).toString();
-        int contador = 0;
-
+    public static boolean bingo (){
         //Revisa del primer número al último en busca de bingo, si encuentra algún
         //valor que no sea un espacio para automáticamente para mayor eficiencia.
-        while (numero.equals("  ") && contador<carton.size()){
-            numero = carton.get(contador).toString();
-            if(numero.equals("  ")){
-                contador++;}
+        for (Object[] linea : carton) {
+            for (Object numero: linea) {
+                if (!numero.equals("  ")){
+                    return false;
+                }
+            }
         }
+        return true;
 
-        //Si completa el ciclo todos los números serán espacios y habrá bingo
-        if (contador==carton.size()){
-            return true;
-        }
 
-        //Si para antes de llegar a 27 comprobaciones todavía quedará algún número
-        //por tachar y no será bingo
-        return false;
     }}
