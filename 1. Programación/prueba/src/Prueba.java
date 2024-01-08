b1034dd509d465fe67ce5d6f2e69c3e140213b91
Jesús <jsus3005@gmail.com>
2024-01-08 14:24:25 +0100

    update

---
+++ 1. Programación/prueba/src/Prueba.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Prueba {
+
+
+    public static void main(String[] args) {
+        int[] alumnos = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
+        ArrayList numerosUsados = new ArrayList<>();
+        int[] mostrar = new int[3];
+
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("De cuanto es el grupo?");
+        int grupo = scanner.nextInt();
+
+        for (int item : alumnos) {
+
+
+        }
+
+        for (int i = 0; i < alumnos.length; i++) {
+
+            for (int j = 0; j < grupo; j++) {
+
+                int numeroRandom = (int)(Math.random()*alumnos.length);
+
+                if (numerosUsados.contains(alumnos[numeroRandom])){
+                    i--;
+                } else {
+
+                    numerosUsados.add(alumnos[numeroRandom]);
+
+                    mostrar[j]=alumnos[numeroRandom];
+                }
+
+
+                mostrar[j]=numeroRandom;
+
+
+            }
+
+            for (int item : mostrar) {
+                System.out.print(item+",");
+
+            }
+            System.out.println();
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
