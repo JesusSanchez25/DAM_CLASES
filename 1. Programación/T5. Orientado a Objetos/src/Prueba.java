c6b0a4b33bdd4eef6292cf11c37c5ea3a1ed9613
Jesús <jsus3005@gmail.com>
2024-01-12 14:12:35 +0100

    update

--- 1. Programación/T5. Orientado a Objetos/src/Prueba.java
+++ 1. Programación/T5. Orientado a Objetos/src/Prueba.java
@@ -1,13 +1,22 @@
-public class Prueba {
-
-    public static boolean isNarcissistic(int number) {
-        // TODO replace with your code
-
-        
-
-        return false;
+public class Prueba
+{
+    public static String reverseWords(final String original)
+    {
+        String[] palabras = original.replaceAll(" ", "").split("ñ");
+        String inversedWord = "";
+        for (String item : palabras) {
+            for (int i = item.length()-1; i >= 0; i--){
+                inversedWord += item.toCharArray()[i];
+            }
+            if (item.equals(palabras[palabras.length-1])){
+                return inversedWord;
+            }
+            inversedWord += " ";
+        }
+        return inversedWord;
     }
 
-
-
+    public static void main(String[] args) {
+        System.out.println(reverseWords("The quick brown fox jumps over the lazy dog."));
+    }
 }
