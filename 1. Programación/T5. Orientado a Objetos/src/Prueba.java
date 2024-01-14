83aa4041c970addace4b51d9e95987f496f8d743
JesusSanchez25 <jsus3005@gmail.com>
2024-01-14 12:51:31 +0100

    update

--- 1. Programación/T5. Orientado a Objetos/src/Prueba.java
+++ 1. Programación/T5. Orientado a Objetos/src/Prueba.java
@@ -2,21 +2,35 @@ public class Prueba
 {
     public static String reverseWords(final String original)
     {
-        String[] palabras = original.replaceAll(" ", "").split("ñ");
+        String[] palabras = original.split(" ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         String inversedWord = "";
         for (String item : palabras) {
             for (int i = item.length()-1; i >= 0; i--){
                 inversedWord += item.toCharArray()[i];
             }
-            if (item.equals(palabras[palabras.length-1])){
-                return inversedWord;
-            }
+
             inversedWord += " ";
         }
         return inversedWord;
     }
 
     public static void main(String[] args) {
-        System.out.println(reverseWords("The quick brown fox jumps over the lazy dog."));
+        System.out.println(reverseWords("Me comí una salchipapa y que cosa tan sabrosa."));
     }
 }
