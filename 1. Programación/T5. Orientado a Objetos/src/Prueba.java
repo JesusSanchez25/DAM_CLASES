9398295133f0c6e4ba7a19468ea0a3a909cd490f
JesusSanchez25 <jsus3005@gmail.com>
2024-01-16 15:46:59 +0100

    update

--- 1. Programación/T5. Orientado a Objetos/src/Prueba.java
+++ 1. Programación/T5. Orientado a Objetos/src/Prueba.java
@@ -1,36 +1,35 @@
-public class Prueba
-{
-    public static String reverseWords(final String original)
-    {
-        String[] palabras = original.split(" ");
+public class Prueba {
+    public static String reverseWords(String sentence) {
+        // Split the sentence into words
+        String[] words = sentence.split(" ");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        String inversedWord = "";
-        for (String item : palabras) {
-            for (int i = item.length()-1; i >= 0; i--){
-                inversedWord += item.toCharArray()[i];
-            }
-
-            inversedWord += " ";
+        // Reverse each word
+        for (int i = 0; i < words.length; i++) {
+            words[i] = reverseWord(words[i]);
         }
-        return inversedWord;
+
+        // Join the reversed words back into a sentence
+        return String.join(" ", words);
+    }
+
+    private static String reverseWord(String word) {
+        // Convert the word to a char array
+        char[] charArray = word.toCharArray();
+
+        // Reverse the char array
+        for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+        }
+
+        // Convert the char array back to a string
+        return new String(charArray);
     }
 
     public static void main(String[] args) {
-        System.out.println(reverseWords("Me comí una salchipapa y que cosa tan sabrosa."));
+        // Test cases
+        System.out.println(reverseWords("            ")); // Output: "sihT si na !elpmaxe"
+        System.out.println(reverseWords("       "));      // Output: "elbuod  secaps"
     }
 }
---
+++ 1. Programación/T5. Orientado a Objetos/src/ejerciciosClase/ejercicioClase7/controler/Partido.java
@@ -0,0 +1,63 @@
+package ejerciciosClase.ejercicioClase7.controler;
+
+import ejerciciosClase.ejercicioClase7.model.Equipo;
+
+import java.util.Random;
+
+public class Partido {
+    private Equipo equipoA;
+    private Equipo equipoB;
+    private boolean partidoEnJuego;
+    private int parte;
+
+    // Constructor que inicializa los equipos y otros atributos
+    public Partido(Equipo equipoA, Equipo equipoB) {
+        this.equipoA = equipoA;
+        this.equipoB = equipoB;
+        this.partidoEnJuego = false;
+        this.parte = 0;
+    }
+
+    // Método para iniciar el partido
+    public void iniciarPartido() {
+        if (!partidoEnJuego) {
+            partidoEnJuego = true;
+
+            // Se juegan tres partes
+            for (int i = 0; i < 3; i++) {
+                // Realizar tres ataques por cada parte
+                realizarAtaque(equipoA, equipoB);
+                realizarAtaque(equipoB, equipoA);
+
+                // Incrementar el número de parte
+                parte++;
+
+                // Mostrar el resultado después de cada parte
+                mostrarResultado();
+            }
+
+            // El partido ha terminado
+            partidoEnJuego = false;
+        } else {
+            System.out.println("El partido ya está en juego.");
+        }
+    }
+
+    private static void realizarAtaque(Equipo atacante, Equipo defensor) {
+        boolean gol = atacante.atacar(defensor);
+        if (gol) {
+            System.out.println("¡Gol! " + atacante.getNombre() + " ha marcado un gol contra " + defensor.getNombre());
+        } else {
+            System.out.println("¡Fallo! " + atacante.getNombre() + " no ha marcado contra " + defensor.getNombre());
+        }
+    }
+
+    // Método para mostrar el resultado final del partido
+    public void mostrarResultado() {
+        System.out.println("Resultado del Partido: " + equipoA.getNombre() + " " + equipoA.getGoles() +
+                " - " + equipoB.getNombre() + " " + equipoB.getGoles());
+    }
+
+    // Otros métodos, getters y setters, según sea necesario
+    // ...
+}
