7c058c993ba6b2ecd5c88e59afcda84a83de1507
Jesús <jsus3005@gmail.com>
2024-01-31 11:33:41 +0100

    update

---
+++ 1. Programación/T7. Uso avanzado/src/model/Estaciones.java
@@ -0,0 +1,45 @@
+package model;
+
+public enum Estaciones {
+
+    // PLANTILLAS
+    invierno ("invierno", 1, false),
+    primavera("primavera", 20, true),
+    verano("verano", 30, true),
+    otonio("otoño", 10, false);
+
+    private String nombre;
+    private int temperaturaMedia;
+    private boolean indiceUV;
+
+    Estaciones(String nombre, int temperaturaMedia, boolean indiceUV) {
+
+        this.nombre = nombre;
+        this.temperaturaMedia = temperaturaMedia;
+        this.indiceUV = indiceUV;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTemperaturaMedia() {
+        return temperaturaMedia;
+    }
+
+    public void setTemperaturaMedia(int temperaturaMedia) {
+        this.temperaturaMedia = temperaturaMedia;
+    }
+
+    public boolean isIndiceUV() {
+        return indiceUV;
+    }
+
+    public void setIndiceUV(boolean indiceUV) {
+        this.indiceUV = indiceUV;
+    }
+}
---
+++ 1. Programación/T7. Uso avanzado/src/model/Registro.java
@@ -0,0 +1,86 @@
+package model;
+
+public class Registro {
+
+    private int temperatura;
+    private int hora, minuto, dia, mes, anio;
+    private Estaciones estacion;
+
+    public Registro() {
+    }
+
+    public Registro(int temperatura, int hora, int minuto, int dia, int mes, int anio, Estaciones estacion) {
+        this.temperatura = temperatura;
+        this.hora = hora;
+        this.minuto = minuto;
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+        this.estacion = estacion;
+
+        if (temperatura > estacion.getTemperaturaMedia()){
+            System.out.println("La temperatura introducida es superior a la media de ese mes");
+            System.out.println("Desea continuar?");
+
+        } else {
+            System.out.println("Temperatura introducida correctamente");
+        }
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public Estaciones getEstacion() {
+        return estacion;
+    }
+
+    public void setEstacion(Estaciones estacion) {
+        this.estacion = estacion;
+    }
+}
+
