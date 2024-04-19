96116eeb880e6856a0f01b823363908a762423db
Jesús <jsus3005@gmail.com>
2024-04-19 14:16:43 +0200

    commit

---
+++ 1. Programación/T9_Base de datos/src/main/java/database/DBconnection.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.Connection;
+
+public class DBconnection {
+
+    Connection conncetion;
+
+    // plantear un patrón SINGLETONE
+
+    public void getConnection (){
+        //retorna la conexión
+    }
+    public void createConnection (){
+        // crea la conexión
+    }
+    public void closeConnection (){
+        // cierra la conexión
+    }
+
+}
---
+++ 1. Programación/T9_Base de datos/src/main/java/database/Esquema.java
@@ -0,0 +1,16 @@
+package database;
+
+public interface Esquema {
+
+    String DB_NAME = "curso";
+    String TAB_ALUMNOS = "alumnos";
+    String TAB_PROFESORES = "profesores";
+    String COL_NOMBRE = "nombre";
+    String COL_APELLIDO = "apellido";
+    String COL_CORREO = "correo";
+    String COL_TELEFONO= "telefono";
+    String COL_ESPECIALIDAD = "especialidad";
+
+
+
+}
