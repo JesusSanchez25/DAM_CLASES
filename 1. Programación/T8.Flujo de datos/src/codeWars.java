df88be356fa517866238db24d989250b56a63f69
JesusSanchez25 <jsus3005@gmail.com>
2024-04-22 21:49:03 +0200

    update

--- 1. Programación/T8.Flujo de datos/src/codeWars.java
+++ 1. Programación/T8.Flujo de datos/src/codeWars.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
 
             public static void main(String[] args) {
 
-                System.out.println(camelCase("camion Neta"));
+                System.out.println(camelCase("<eye Other [Young Thing Come Last Different Number] Year>"));
             }
             public static String camelCase(String input) {
                 String input2 = "";
                 char[] array = input.toCharArray();
 
-
-                for (char letra : array) {
-                    if (Character.isUpperCase(letra)){
-                        input = input.replace(String.valueOf(letra), " " + letra);
+                for (int i = 1; i < array.length; i++) {
+                    if (Character.isUpperCase(array[i]) && Character.isLowerCase(array[i-1])){
+                        input = input.replace(String.valueOf(array[i]), " " + array[i]);
                     }
                 }
 
